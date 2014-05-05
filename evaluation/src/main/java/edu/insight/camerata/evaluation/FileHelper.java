434c2bf685881b2f7060725617cfc6892ca17920
Kartik <asooja@gmail.com>
2014-05-05 15:48:44 +0100

    MusicXMLDemo code and some sample xml files

---
+++ evaluation/src/main/java/edu/insight/camerata/evaluation/FileHelper.java
@@ -0,0 +1,56 @@
+package edu.insight.camerata.evaluation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class FileHelper {
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println("START");
+			String outputPath = "/Users/Scott/workspace/myrepo/my-app/output/text.txt";
+			FileHelper fileHelper = new FileHelper();
+			fileHelper.write(new File(outputPath), "Hello World! My First HTML Page!");
+			String string = fileHelper.read(new File(outputPath));
+			System.out.println("From FILE:"+string);
+			System.out.println("FINISH");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public String read(File inputFile) throws Exception {
+		// Initialization
+		String string = "";
+		String line;
+		
+		// Read from file
+		FileReader fr = new FileReader(inputFile);
+		BufferedReader br = new BufferedReader(fr);
+		while ((line = br.readLine()) != null) {
+			string = string + line;
+		}
+		br.close();
+		fr.close();
+		
+		return string;
+	}
+
+	public void write(File outputFile, String string) throws Exception {
+		// Initialization
+		if (!outputFile.exists()) {
+			outputFile.createNewFile();
+		}
+
+		// Write to file
+		FileWriter fw = new FileWriter(outputFile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(string);
+		bw.close();
+		fw.close();
+
+	}
+}
---
+++ evaluation/src/main/java/edu/insight/camerata/evaluation/MusicXMLDemo.java
@@ -0,0 +1,62 @@
+package edu.insight.camerata.evaluation;
+
+import java.io.File;
+import org.jfugue.MusicStringParser;
+import org.jfugue.MusicStringRenderer;
+import org.jfugue.MusicXmlParser;
+import org.jfugue.MusicXmlRenderer;
+import org.jfugue.Pattern;
+import org.jfugue.Player;
+
+public class MusicXMLDemo {
+
+	public static void main(String[] args) { 
+		try {
+			System.out.println("START");
+			//String inputFile = "src/main/resources/Tonight.xml";
+			String inputFile = "src/main/resources/DebuMandSample.xml";
+			
+
+			String outputFile = "src/main/resources/mxmlfhout.xml";
+			Pattern pattern = new Pattern("KEmaj V0 E4i+B4i B3i E4i+B4i B3i E4i+B4i B3i E4i+B4i B3i V1 E1h E1i F#1i G#1i B1i");
+
+			MusicXMLDemo fileHelper = new MusicXMLDemo();
+			Pattern readPatt = fileHelper.read(new File(inputFile));
+
+			//	fileHelper.write(new File(outputFile), pattern);
+
+			Player player = new Player();
+			System.out.println(readPatt.getMusicString());
+			player.play(readPatt);
+			//player.play(pattern);
+			System.out.println("FINISH");
+		} catch (Exception ex) {
+
+		}
+	}
+
+	public void write(File outputFile, Pattern musicPattern) throws Exception {
+		MusicXmlRenderer renderer = new MusicXmlRenderer();
+
+		MusicStringParser parser = new MusicStringParser();
+		parser.addParserListener(renderer);
+		parser.parse(musicPattern);
+
+		FileHelper fileHelper = new FileHelper();
+		fileHelper.write(outputFile, renderer.getMusicXMLString());
+	}
+
+	public Pattern read(File inputFile) throws Exception{
+
+		MusicStringRenderer renderer = new MusicStringRenderer();
+
+		MusicXmlParser parser = new MusicXmlParser();
+
+		parser.addParserListener(renderer);
+		parser.parse(inputFile);
+
+		return new Pattern(renderer.getPattern());
+	}
+
+	
+}
