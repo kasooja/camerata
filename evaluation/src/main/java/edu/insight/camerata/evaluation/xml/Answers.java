c0833496a40631310f11dcd18f5228b0483e3d03
sindhukiranmai <eskiranmai94@gmail.com>
2014-06-17 14:25:13 +0100

    Answers.java : updated until end_divisions

--- evaluation/src/main/java/edu/insight/camerata/evaluation/xml/Answers.java
+++ evaluation/src/main/java/edu/insight/camerata/evaluation/xml/Answers.java
@@ -1,58 +1,59 @@
 package edu.insight.camerata.evaluation.xml;
 
-import java.util.Set;
-
 import org.xml.sax.helpers.DefaultHandler;
 
 
 public class Answers {
 	String start_time_sig;
 	String end_time_sig;
-	String start_divs;
-	String end_divs;
+	double start_divs;
+	double end_divs;
 
 	public static Music music1 = new Music();
-	public static String dataPath1 =  "src/main/resources/data/training/f5.xml";
+	public static String dataPath1 =  "src/main/resources/data/training/f4.xml";
 
-	Part first;
-	Measure first1;
-	Part end;
-	Measure end1;
+	Part first_part;
+	Measure first_measure;
+	Part end_part;
+	Measure end_measure;
 
 
 	public void get_ans(){
 
 		// To fetch start time signature
 		for (String key : music1.musicPartMap.keySet()) {
-			first = music1.musicPartMap.get(key);
+			first_part = music1.musicPartMap.get(key);
 			break;
 		}
-		for (String key : first.measures.keySet()) {
-			first1 = first.measures.get(key);
+		for (String key : first_part.measures.keySet()) {
+			first_measure = first_part.measures.get(key);
 			break;
 		}
-		start_time_sig = first1.attributes.time.beats + "/" + first1.attributes.time.beatType;
+		start_time_sig = first_measure.attributes.time.beats + "/" + first_measure.attributes.time.beatType;
 		System.out.println("Start Time Signature : " + start_time_sig);
 
 		// To fetch end time signature
 		for (String key : music1.musicPartMap.keySet()) {
-			end = music1.musicPartMap.get(key);
+			end_part = music1.musicPartMap.get(key);
 		}
-		for (String key : end.measures.keySet()) {
-			end1 = end.measures.get(key);
+		for (String key : end_part.measures.keySet()) {
+			end_measure = end_part.measures.get(key);
 			break;
 		}
-		end_time_sig = end1.attributes.time.beats + "/" + end1.attributes.time.beatType;
+		end_time_sig = end_measure.attributes.time.beats + "/" + end_measure.attributes.time.beatType;
 		System.out.println("End Time Signature : " + end_time_sig);		
 
 		// To fetch start divisions
+		start_divs = first_measure.attributes.time.beatType / 4;
+		System.out.println("Start Divisions : " + start_divs);		
 
-		double start_no_divisions = first1.attributes.divisions;
-		double count = 0;
-		for (Integer key : first1.notes.keySet()){
-			Note temp = first1.notes.get(key);
-			count = count + temp.duration;
-		}
+		// To fetch end divisions
+		end_divs = end_measure.attributes.time.beatType / 4;
+		System.out.println("End Divisions : " + end_divs);		
+
+
+		
+		
 	}
 
 	public static void main(String []args){
